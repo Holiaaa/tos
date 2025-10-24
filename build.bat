c068563ae6133d44afe8fd2e6023ca6230613079
Téo J. <105713570+Holiaaa@users.noreply.github.com>
2025-10-24 16:06:07 +0200

    Add files via upload

---
+++ build.bat
@@ -0,0 +1,9 @@
+:: Teo Operating System Project
+:: build file
+
+@mkdir build -Force
+@i686-elf-gcc -m16 -ffreestanding -nostdlib -Iinclude -c sources/main.c -o build/main.o
+@i686-elf-gcc -m16 -ffreestanding -nostdlib -Iinclude -c sources/io.c -o build/io.o
+@i686-elf-ld -Ttext 0x7C00 --oformat binary -e _start build/main.o build/io.o -o build/os.bin
+@powershell scripts\make_bootimage.ps1
+@qemu-system-i386 -fda build\final.bin
