4d0bd71d810f8f839807fe65e5e1c3639e598d4a
Téo J. <105713570+Holiaaa@users.noreply.github.com>
2025-10-24 18:16:19 +0200

    Delete scripts directory

--- scripts/make_bootimage.ps1
+++
@@ -1,7 +0,0 @@
-# This file complete the bootloader to have 510 '0' and 0x55AA as the last bytes.
-
-$bin = [System.IO.File]::ReadAllBytes("build/os.bin")
-$pad = New-Object Byte[] (510 - $bin.Length)
-$sig = [Byte[]](0x55,0xAA)
-$final = $bin + $pad + $sig
-[System.IO.File]::WriteAllBytes("build/final.bin", $final)
